ea9b2f130c7a5530983efba20a4b3a97cf4d09fb
KARTIK14085 <kartik123489@gmail.com>
2026-06-17 09:25:18 +0530

    django workshop first commit

---
+++ myuserapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.homepage),
+    path('home', views.homepage),
+    path('about', views.about),
+    path('contact', views.contact),
+    path('shop', views.shop),
+    
+]
